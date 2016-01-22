74fba211759cfa552f71f936b44c1eecdf5b01a4
Lammert Westerhoff <westerhoff@gmail.com>
2016-01-22 14:38:40 +0100

    Change data

--- marketing-quiz.js
+++ marketing-quiz.js
@@ -91,30 +91,33 @@ var marketingQuiz = (function () {
               "answer": "Yes",
               "scores": {
                 "Native": 2,
-                "Xamarin": 0,
-                "Web": -1
+                "Xamarin": 1,
+                "React Native": 1,
+                "Hybrid": 0,
+                "Web": -1,
               }
             },
             {
               "id": "2",
               "answer": "No",
               "scores": {
-                "Native": 2,
+                "Native": -1,
                 "Xamarin": 0,
-                "Web": -1
-              },
-              "excludes": ["2.1"
-              ]
+                "React Native": 0,
+                "Hybrid": 1,
+                "Web": 1,
+              }
             },
             {
               "id": "2",
               "answer": "Not sure",
               "scores": {
-                "Native": 2,
-                "Xamarin": 0,
-                "Web": -1
-              },
-              "excludes": []
+                "Native": 0,
+                "Xamarin": 1,
+                "React Native": 1,
+                "Hybrid": 0,
+                "Web": 0,
+              }
             }
           ],
           "type": "radio"
@@ -127,9 +130,11 @@ var marketingQuiz = (function () {
               "id": "1",
               "answer": "Phone",
               "scores": {
-                "Native": 2,
-                "Xamarin": 0,
-                "Web": -1
+                "Native": 1,
+                "Xamarin": 1,
+                "React Native": 1,
+                "Hybrid": 1,
+                "Web": 1,
               }
             },
             {
@@ -137,12 +142,11 @@ var marketingQuiz = (function () {
               "answer": "Tablet",
               "scores": {
                 "Native": 2,
-                "Xamarin": 0,
-                "Web": -1
-              },
-              "excludes": [
-                "2.1"
-              ]
+                "Xamarin": 2,
+                "React Native": 2,
+                "Hybrid": 1,
+                "Web": 0,
+              }
             },
             {
               "id": "2",
@@ -150,100 +154,107 @@ var marketingQuiz = (function () {
               "scores": {
                 "Native": 2,
                 "Xamarin": 0,
-                "Web": -1
-              },
-              "excludes": [
-                "2.1"
-              ]
+                "React Native": 0,
+                "Hybrid": 1,
+                "Web": 2,
+              }
             }
           ],
           "type": "radio"
         },
-        {
-          "id": "3",
-          "question": "Which platforms do you want to support?",
-          "answers": [
-            {
-              "id": "1",
-              "answer": "iOS",
-              "scores": {
-                "Native": 2,
-                "Xamarin": 0,
-                "Web": -1
-              }
-            },
-            {
-              "id": "2",
-              "answer": "Android",
-              "scores": {
-                "Native": 2,
-                "Xamarin": 0,
-                "Web": -1
-              },
-              "excludes": [
-                "2.1"
-              ]
-            },
-            {
-              "id": "3",
-              "answer": "Windows",
-              "scores": {
-                "Native": 2,
-                "Xamarin": 0,
-                "Web": -1
-              },
-              "excludes": [
-                "2.1"
-              ]
-            },
-            {
-              "id": "4",
-              "answer": "Not Shure Yet",
-              "scores": {
-                "Native": 2,
-                "Xamarin": 0,
-                "Web": -1
-              },
-              "excludes": [
-                "2.1"
-              ]
-            }
-          ],
-          "type": "checkbox"
-        },
-        {
-          "id": "4",
-          "question": "Please mark all categories that describe your app?",
-          "answers": [
-            {
-              "id": "1",
-              "answer": "Puzzle Game"
-            },{
-              "id": "2",
-              "answer": "3D Game"
-            },{
-              "id": "3",
-              "answer": "Social App"
-            },{
-              "id": "4",
-              "answer": "Productivity"
-            },{
-              "id": "5",
-              "answer": "Promotional"
-            },{
-              "id": "6",
-              "answer": "Business"
-            },{
-              "id": "7",
-              "answer": "Education"
-            },{
-              "id": "8",
-              "answer": "Travel"
-            }
+        // {
+        //   "id": "3",
+        //   "question": "Which platforms do you want to support?",
+        //   "answers": [
+        //     {
+        //       "id": "1",
+        //       "answer": "iOS",
+        //       "scores": {
+        //         "Native": ,
+        //         "Xamarin": ,
+        //         "React Native": ,
+        //         "Hybrid": ,
+        //         "Web": -,
+        //       }
+        //     },
+        //     {
+        //       "id": "2",
+        //       "answer": "Android",
+        //       "scores": {
+        //         "Native": ,
+        //         "Xamarin": ,
+        //         "React Native": ,
+        //         "Hybrid": ,
+        //         "Web": -,
+        //       },
+        //       "excludes": [
+        //         "2.1"
+        //       ]
+        //     },
+        //     {
+        //       "id": "3",
+        //       "answer": "Windows",
+        //       "scores": {
+        //         "Native": ,
+        //         "Xamarin": ,
+        //         "React Native": ,
+        //         "Hybrid": ,
+        //         "Web": -,
+        //       },
+        //       "excludes": [
+        //         "2.1"
+        //       ]
+        //     },
+        //     {
+        //       "id": "4",
+        //       "answer": "Not Shure Yet",
+        //       "scores": {
+        //         "Native": ,
+        //         "Xamarin": ,
+        //         "React Native": ,
+        //         "Hybrid": ,
+        //         "Web": -,
+        //       },
+        //       "excludes": [
+        //         "2.1"
+        //       ]
+        //     }
+        //   ],
+        //   "type": "checkbox"
+        // },
+        // {
+        //   "id": "4",
+        //   "question": "Please mark all categories that describe your app?",
+        //   "answers": [
+        //     {
+        //       "id": "1",
+        //       "answer": "Puzzle Game"
+        //     },{
+        //       "id": "2",
+        //       "answer": "3D Game"
+        //     },{
+        //       "id": "3",
+        //       "answer": "Social App"
+        //     },{
+        //       "id": "4",
+        //       "answer": "Productivity"
+        //     },{
+        //       "id": "5",
+        //       "answer": "Promotional"
+        //     },{
+        //       "id": "6",
+        //       "answer": "Business"
+        //     },{
+        //       "id": "7",
+        //       "answer": "Education"
+        //     },{
+        //       "id": "8",
+        //       "answer": "Travel"
+        //     }
 
-          ],
-          "type": "checkbox"
-        },
+        //   ],
+        //   "type": "checkbox"
+        // },
         {
           "id": "1",
           "question": "Which description best describes your app",
@@ -252,59 +263,101 @@ var marketingQuiz = (function () {
               "id": "1",
               "answer": "It’s mainly tables with text and numbers",
               "icon": "phone_tables.png",
-              "color": "#2196F3"
+              "color": "#2196F3",
+              "scores": {
+                "Native": 1,
+                "Xamarin": 2,
+                "React Native": 2,
+                "Hybrid": 1,
+                "Web": 0,
+              }
             },{
               "id": "2",
               "answer": "The users need to fill in forms and submit data",
               "icon": "phone_form.png",
-              "color": "#CDDC39"
+              "color": "#CDDC39",
+              "scores": {
+                "Native": 0,
+                "Xamarin": 3,
+                "React Native": 1,
+                "Hybrid": 2,
+                "Web": 2,
+              }
             },{
               "id": "3",
               "answer": "A lot of custom graphics and animations",
               "icon": "phone_graphics.png",
-              "color": "#FF5722"
+              "color": "#FF5722",
+              "scores": {
+                "Native": 2,
+                "Xamarin": 0,
+                "React Native": 0,
+                "Hybrid": 1,
+                "Web": 1,
+              }
             },{
               "id": "4",
               "answer": "It only has one or two screens",
               "icon": "phone_two_screens.png",
-              "color": "#E91E63"
+              "color": "#E91E63",
+              "scores": {
+                "Native": 0,
+                "Xamarin": 1,
+                "React Native": 1,
+                "Hybrid": 2,
+                "Web": 2,
+              }
             },{
               "id": "5",
               "answer": "It contains the same information as our (corporate) website",
               "icon": "phone_web.png",
-              "color": "#FFC107"
+              "color": "#FFC107",
+              "scores": {
+                "Native": 0,
+                "Xamarin": 0,
+                "React Native": 1,
+                "Hybrid": 2,
+                "Web": 3,
+              }
             },{
               "id": "6",
               "answer": "None of the others",
               "icon": "cross.png",
-              "color": "#9E9E9E"
+              "color": "#9E9E9E",
+              "scores": {
+                "Native": 3,
+                "Xamarin": 2,
+                "React Native": 1,
+                "Hybrid": 0,
+                "Web": 0,
+              }
             }
           ],
           "type": "checkbox"
         },
-        {
-          "id": "2",
-          "question": "Which of the following features does your app need?",
-          "answers": [
-            {
-              "id": "1",
-              "answer": "Maps"
-            },{
-              "id": "2",
-              "answer": "Microphone"
-            },{
-              "id": "3",
-              "answer": "Push Notifications"
-            },{
-              "id": "4",
-              "answer": "Offline Mode"
-            },{
-              "id": "5",
-              "answer": "Data storage on device"
-            }
-          ],
-          "type": "checkbox"
-        }
+        // {
+        //   "id": "2",
+        //   "question": "Which of the following features does your app need?",
+        //   "answers": [
+        //     {
+        //       "id": "1",
+        //       "answer": "Maps"
+        //     },{
+        //       "id": "2",
+        //       "answer": "Microphone"
+        //     },{
+        //       "id": "3",
+        //       "answer": "Push Notifications"
+        //     },{
+        //       "id": "4",
+        //       "answer": "Offline Mode"
+        //     },{
+        //       "id": "5",
+        //       "answer": "Data storage on device"
+        //     }
+        //   ],
+        //   "type": "checkbox"
+        // }
       ],
       "results": [
         {
